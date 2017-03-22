8373dceb200bcec67c52a1084caf93b987328132
bkelemen56 <bill@kelemen-usa.com>
2017-03-22 11:15:54 -0400

    added logging via futil.logger

--- R/01-make_model_part_1.R
+++ R/01-make_model_part_1.R
@@ -1,57 +1,90 @@
-debugSource('src/calc_models_4_4.R')
+# ---------------------------------------------------------------------
+# model #5.0 - final model
+# make model - part 1
+#
+# script to process the training dataset and train 80 separate models,
+# each on 1% of the total data. these separate models will be combined
+# later via the make_model_part_2 script into the final model. 
+# ---------------------------------------------------------------------
 
-model_id    <- "model-4-4"
-# use_small_data <- FALSE
-debug       <- FALSE           # to only test loops, no file processing
-do_parallel <- TRUE           # run in parallel (no console output) - only in production
-n_prune     <- 4              # number of words to keep for same root
+source('R/globals.R')
+source('R/model_util.R')
 
-setDTthreads(7)                # for data.table parallel processing
+# ---------------------------------------------------------------------
+# parameters
+# ---------------------------------------------------------------------
 
-# process incrementally a training (small) file
-process_model <- function(model_fname, 
-                           n_prune = 10, 
-                           do_mem_size_calc = FALSE,
-                           debug_model_calc = FALSE) {
-  
-  text <- load_documents0(model_fname) %>% 
-    clean_data()
-  
-  #text <- text[[1]]
-  
-  model <- calc_model(text, model_fname, n_prune = n_prune, do_mem_size_calc = do_mem_size_calc)
-}
+use_small_data <- FALSE
+debug          <- TRUE          # to only test loops, no file processing
+do_parallel    <- FALSE           # run in parallel (no console output) - only in production
+n_prune        <- 5              # number of words to keep for same root
 
-format_fixed_width <- function(i, width) {
-  s <- paste(i)
-  while (str_length(s) < width) s <- paste0("0", s)
-  s
-}
+# ---------------------------------------------------------------------
+# functions
+# ---------------------------------------------------------------------
 
+#' creates the model from one of the training files.
+#' 
+#' \code{process_file} reads one of the training files, cleans the 
+#'   text data, calculates the model and saved it to cache. This
+#'   function is intended to be called in a for loop to process all
+#'   80 sections of the training dataset.
+#'
+#' @param i document number to read
+#' @return NONE 
+#' @examples
+#'   \code{process_file(1)} will read \code{all.train.001.txt} from
+#'   the PATH_DATA folder and create \code{model_XXX.001.cache} in
+#'   the PATH_CACHE folder.
+#'   
 process_file <- function(i) {
-  cat(paste("processing file #", format_fixed_width(i, 3), "\n"))
-  model <- process_model(paste0("data/all.train-", format_fixed_width(i, 3), ".txt"), 
-                         n_prune, 
-                         do_mem_size_calc = TRUE)
-  save_to_cache(model, paste0(model_id, ".", format_fixed_width(i, 3), ".cache"))
-  NULL
+  file_number <- str_pad(i, 3, side = "left", "0")
+  doc_filename <- paste0("all.train-", file_number, ".txt")
+  model_filename <- paste0(MODEL_ID, ".", file_number, ".cache")
+  
+  flog.info(paste("processing file", doc_filename))
+  
+  load_documents(doc_filename) %>% 
+    clean_documents() %>%
+    calc_model(n_prune = n_prune, do_mem_size_calc = TRUE) %>%
+    save_model_to_cache(model_filename)
+
+  flog.info(paste("completed processing file", doc_filename))
+  
+  invisible()
 }
 
-if (do_parallel) {
-  cat("computing in parallel... (no console output)")
+# ---------------------------------------------------------------------
+# main
+# ---------------------------------------------------------------------
+
+# initializde the logger and start logging...
+init_logger(threshold = DEBUG, filename = "make_model_part_1", timestamp = TRUE, tee = TRUE)
+flog.info("start: make_model_part_1")
+
+if (debug) {
+  flog.info("start: debug computing")
+  process_file(1)
+  flog.info("end: debug computing")
+  
+} else if (do_parallel) {
+  flog.info("start: parallel computing")
   library(doParallel)
   
   no_cores <- detectCores() - 1  
   cl <- makeCluster(no_cores, type = "FORK")  
   registerDoParallel(cl) # ?no_cores)  
 
+  flog.info(paste("computing with", no_cores, "cores"))
   foreach(i = 1:80) %dopar% process_file(i)
 
   stopCluster(cl)
-  
-} else if (debug) {
-  process_file(1)
+  flog.info("end: parallel computing")
   
 } else {
+  flog.info("start: sequential computing")
   for (i in c(1:80)) process_file(i)
+  flog.info("end: sequential computing")
 }
+
+flog.info("end: make_model_part_1")

8a22876bd5a147fa9a9faa7b4da75adbb5f8ecaa
bkelemen56 <bill@kelemen-usa.com>
2017-03-23 17:41:17 -0400

    implemented best model accuracy calc

--- R/05-calc_accuracy.R
+++ R/05-calc_accuracy.R
@@ -28,9 +28,10 @@ n_lines_to_process <- 200
 # parameters to predict word
 use_unigram_model <- TRUE
 discount_factor <- 0.5
+max_model_level <- 4
 
 # use_small_data <- FALSE
-debug       <- TRUE           # to only test loops, no file processing
+debug       <- FALSE           # to only test loops, no file processing
 do_parallel <- TRUE           # run in parallel (no console output) - only in production
 n_prune     <- 5              # number of words to keep for same root
 
@@ -42,10 +43,41 @@ n_prune     <- 5              # number of words to keep for same root
 init_logger(threshold = DEBUG, filename = "calc_accuracy", timestamp = TRUE, tee = TRUE)
 flog.info("start: calc_accuracy")
 
-# we could iterate here over various models...
-calc_model_accuracy(model_fname, 
-                    n_lines_to_process = ifelse(debug, 100, n_lines_to_process),
-                    discount_factor = discount_factor, 
-                    use_unigram_model = use_unigram_model)
+cat('calculate best model accuracy:\n\n')
+
+# load model from cache
+postfix <- readline(prompt = "which models to test [a-g] /a-g/? ")
+if (postfix == "") postfix <- "abcdefg"
+
+flog.info(paste0("use_unigram_model = ", use_unigram_model))
+flog.info(paste0("discount_factor = ", discount_factor))
+flog.info(paste0("max_model_level = ", max_model_level))
+
+best_accuracy <- list(accuracy = 0)
+best_model <- NULL
+results <- data.table(model_fname = character(), accuracy = double())
+
+for (ch in tokenize_characters(postfix)[[1]]) {
+  model_fname <- paste0(MODEL_ID, ".001-", ch, ".cache")
+  accuracy <- calc_model_accuracy(model_fname, 
+                                  n_lines_to_process = ifelse(debug, 10, n_lines_to_process),
+                                  discount_factor = discount_factor, 
+                                  use_unigram_model = use_unigram_model,
+                                  max_model_level = max_model_level)
+
+  if (accuracy$accuracy > best_accuracy$accuracy) {
+    best_model <- model_fname
+    best_accuracy <- accuracy
+  }
+  
+  # save results
+  results <- rbind(results, 
+                   data.table(model_fname = model_fname, accuracy = accuracy$accuracy))
+}
+
+flog.info(paste0("best model = ", best_model))
+log_accuracy("best accuracy:", best_accuracy)
+
+flog.info("all accuracies:", results, capture = TRUE)
 
 flog.info("end: calc_accuracy")
